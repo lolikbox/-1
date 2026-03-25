4ad1559a953885b649dfbc70389b3101fb17720f
lolikbox <tixonsva@gmail.com>
2026-03-25 11:30:51 +0000

    Изменение 1

--- main.py
+++ main.py
@@ -1,90 +1,115 @@
-#1 способ
+#ЗАДАНИЕ ЛР_3 СЕКРЕТНЫЕ КЛЮЧИ 1 часть
+import os
+my_secret_1=os.environ['MY_SECRET']
+my_secret_2=os.environ['MY_SECRET_2']
+my_secret_3=os.environ['TAZHIBAEV_1']
+my_secret_4=os.environ['TAZHIBAEV_2']
+
+print(my_secret_1,my_secret_2,my_secret_3,my_secret_4)
+
 from sympy import *
+import pandas as pd
+import matplotlib.pyplot as plt
+#2 часть задания (Проверяет Виктор)
+##  ИНДИВИДУАЛЬНАЯ ЧАСТЬ ЗАДАНИЯ (3 Вариант)
 k, T, C, L = symbols('k T C L')
-C_ost = 100000
+C_ost = 20000
 Am_lst, C_ost_lst = [], []
-for i in range(5):
-    Am = (C - L) / T
-    val = float(Am.subs({C: 100000, T: 5, L: 0}))
+for i in range(6):
+    Am = (C - L) / T #Что это означает?
+    # формула расчета платежа # Правильно
+    val = float(Am.subs({C: 20000, T: 6, L: 0}))
     C_ost -= val
     Am_lst.append(round(val, 2))
     C_ost_lst.append(round(float(C_ost), 2))
 print('Am_lst=', Am_lst)
 print('C_ost_lst=', C_ost_lst)
+#Все правильно 5, изменилось C_ost = 20000 и T = 6
 
 #2 способ
 Aj = 0
-C_ost = 100000
+C_ost = 20000
 Am_lst_2 = []
 C_ost_lst_2 = []
-for i in range(5):
-    Am_expr = k * 1 / T * (C - Aj)
-    # Вычисляем числовое значение амортизации для текущего года
-    Am_val = float(Am_expr.subs({C: 100000, T: 5, k: 2}))
-    C_ost -= Am_val
-    Am_lst_2.append(round(Am_val, 2))
-    # Накапливаем сумму амортизации Aj как число
-    Aj += Am_val
-    C_ost_lst_2.append(round(float(C_ost), 2))
+for i in range(6):
+    Am = k * 1 / T * (C - Aj)
+    C_ost -= Am.subs({C: 20000, T: 6, k: 2})
+    Am_lst_2.append(round(Am.subs({C: 20000, T: 6, k: 2}), 2))
+    Aj += Am
+    C_ost_lst_2.append(round(C_ost, 2))
 print('Am_lst_2=', Am_lst_2)
 print('C_ost_lst_2=', C_ost_lst_2)
+#Все правильно 5, изменилось C_ost = 20000 и T = 6
+
+
 
 #Табличный вывод
-import pandas as pd
-Y = list(range(1, 6))
+Y = range(1, 7) #Что это означает?
+# цикл от 1 о 7  Правильно
 table1 = list(zip(Y, C_ost_lst, Am_lst))
 table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
-tfame = pd.DataFrame(table1, columns=['Y', 'C_ost', 'Am_lst'])
-tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost', 'Am_lst_2'])
+tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
+tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
 print(tfame)
 print(tfame2)
 
 #Контейнер визуализации
-import matplotlib.pyplot as plt
-
-# График 1: Остаточная стоимость (Способ 1)
+plt.plot(tfame['Y'], tfame['C_ost_lst'], label='Am')
+plt.savefig('chart2_1.png')
 plt.figure()
-plt.plot(tfame['Y'], tfame['C_ost'], marker='o', label='Остаточная стоимость (1)')
-plt.title('Динамика остаточной стоимости (Способ 1)')
-plt.xlabel('Год')
-plt.ylabel('Стоимость')
-plt.grid(True)
-plt.legend()
-plt.savefig('chart1.png')
-
-# График 2: Остаточная стоимость (Способ 2)
+plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am2')
+plt.savefig('chart2_2.png')
 plt.figure()
-plt.plot(tfame2['Y'], tfame2['C_ost'], marker='s', color='r', label='Остаточная стоимость (2)')
-plt.title('Динамика остаточной стоимости (Способ 2)')
-plt.xlabel('Год')
-plt.ylabel('Стоимость')
-plt.grid(True)
-plt.legend()
-plt.savefig('chart2.png')
 
-# График 3: Пироговая диаграмма амортизации (Способ 1)
-labels = [f'Год {x}' for x in range(1, 6)]
-explode = (0.1, 0, 0, 0, 0)
-fig3, ax3 = plt.subplots()
-ax3.pie(Am_lst, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-ax3.set_title('Доля амортизации по годам (Способ 1)')
-plt.savefig('chart3.png')
-
-# График 4: Пироговая диаграмма амортизации (Способ 2)
-fig4, ax4 = plt.subplots()
-ax4.pie(Am_lst_2, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-ax4.set_title('Доля амортизации по годам (Способ 2)')
-plt.savefig('chart4.png')
-
-# График 5: Сравнение амортизации (Столбчатая диаграмма)
+vals = Am_lst
+labels = [str(x) for x in range(1, 7)]
+#Все правильно str(x) for x in range(1, 7), 5
+explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1) #Что это означает? #Список из чисел #Почти правильно
+fig, ax = plt.subplots()
+ax.pie(vals,
+       labels=labels,
+       autopct='%1.1f%%',
+       shadow=True,
+       explode=explode,
+       wedgeprops={
+           'lw': 1,
+           'ls': '--',
+           'edgecolor': "k"
+       },
+       rotatelabels=True)
+ax.axis('equal')
+plt.savefig('chart2_3.png')
 plt.figure()
-x = [y - 0.2 for y in Y]
-x2 = [y + 0.2 for y in Y]
-plt.bar(x, Am_lst, width=0.4, label='Способ 1', align='center')
-plt.bar(x2, Am_lst_2, width=0.4, label='Способ 2', align='center')
-plt.title('Сравнение ежегодной амортизации')
-plt.xlabel('Год')
-plt.ylabel('Сумма амортизации')
-plt.xticks(Y)
-plt.legend()
-plt.savefig('chart5.png')
+
+vals = Am_lst_2
+labels = [str(x) for x in range(1, 7)] #Что это означает? #Список из чисел от одного до семи
+#Все правильно 5
+tt1=20
+explode = (0.1, 0.1, 0.1, 0.1, 0.1,0.1)
+fig, ax = plt.subplots() 
+ax.pie(vals,
+       explode=explode,
+       labels=labels,
+       autopct='%1.1f%%',
+       shadow=True,
+       wedgeprops={
+           'lw': 1,
+           'ls': '--',
+           'edgecolor': "k"
+       },
+       rotatelabels=True)
+ax.axis('equal')
+plt.savefig('chart2_4.png')
+plt.figure()
+
+plt.figure()
+table1 = list(zip(Y, Am_lst))
+table2 = list(zip(Y, Am_lst_2))
+tfame = pd.DataFrame(table1, columns=['Y', 'Am_lst'])
+tfame2 = pd.DataFrame(table2, columns=['Y', 'Am_lst_2'])
+plt.bar(tfame['Y'], tfame['Am_lst'])
+plt.savefig('chart2_5.png')
+plt.figure()
+plt.bar(tfame['Y'], tfame2['Am_lst_2'])
+plt.savefig('chart2_6.png')
+plt.figure()
