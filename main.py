bded2d93ca8bba3495405d9819240dbed5e0c65d
lolikbox <tixonsva@gmail.com>
2026-04-01 22:05:40 +0000

    Изменение2_ИЗ

--- main.py
+++ main.py
@@ -1,116 +1,188 @@
-#ЗАДАНИЕ ЛР_3 СЕКРЕТНЫЕ КЛЮЧИ 1 часть
+# ЛР_3
 import os
-my_secret_1=os.environ['MY_SECRET']
-my_secret_2=os.environ['MY_SECRET_2']
-my_secret_3=os.environ['TAZHIBAEV_1']
-my_secret_4=os.environ['TAZHIBAEV_2']
-
-print(my_secret_1,my_secret_2,my_secret_3,my_secret_4)
-
-from sympy import *
+import random
+import time
+from sympy import symbols
 import pandas as pd
 import matplotlib.pyplot as plt
-#2 часть задания (Проверяет Виктор)
-##  ИНДИВИДУАЛЬНАЯ ЧАСТЬ ЗАДАНИЯ (3 Вариант)
-k, T, C, L = symbols('k T C L')
-C_ost = 50000
-Am_lst, C_ost_lst = [], []
-for i in range(9):
-    Am = (C - L) / T #Что это означает?
-    # формула расчета платежа # Правильно
-    val = float(Am.subs({C: 50000, T: 9, L: 0}))
-    C_ost -= val
-    Am_lst.append(round(val, 2))
-    C_ost_lst.append(round(float(C_ost), 2))
-print('Am_lst=', Am_lst)
-print('C_ost_lst=', C_ost_lst)
-#Все правильно 5, изменилось C_ost = 20000 и T = 6
+import numpy as np
 
-#2 способ
-Aj = 0
-C_ost = 50000
-Am_lst_2 = []
-C_ost_lst_2 = []
-for i in range(9):
-    Am = k * 1 / T * (C - Aj)
-    C_ost -= Am.subs({C: 50000, T: 9, k: 2})
-    Am_lst_2.append(round(Am.subs({C: 50000, T: 9, k: 2}), 2))
-    Aj += Am
-    C_ost_lst_2.append(round(C_ost, 2))
-print('Am_lst_2=', Am_lst_2)
-print('C_ost_lst_2=', C_ost_lst_2)
-#Все правильно 5, изменилось C_ost = 20000 и T = 6
+registry_user= os.environ.get("REGISTRY_USER")
+registry_password= os.environ.get("REGISTRY_PASSWORD")
+sign_key= os.environ.get("SIGN_KEY")
+db_user= os.environ.get("DB_USER")
+dp_pass= os.environ.get("DB_PASS")
 
 
+x,y = symbols('x y')
 
-#Табличный вывод
-Y = range(1, 7) #Что это означает?
-# цикл от 1 о 7  Правильно
-table1 = list(zip(Y, C_ost_lst, Am_lst))
-table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
-tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
-tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
-print(tfame)
-print(tfame2)
+N=1500
+invalid_rate=0.08
+valid_count=0
+invalid_count=0
+processing_times=[]
 
-#Контейнер визуализации
-plt.plot(tfame['Y'], tfame['C_ost_lst'], label='Am')
-plt.savefig('chart2_1.png')
+for i in range (N):
+  start=time.perf_counter()*1000
+  if random.random()<invalid_rate:
+    invalid_count+=1
+  else:
+    valid_count+=1
+
+avg_time= sum(processing_times)/N
+detection_rate=(invalid_count/N)*100 if N else 0
+
+print(f"Всего транзакций: {N}")
+print(f"Количество валидных транзакций: {valid_count}")
+print(f"Количество невалидных транзакций: {invalid_count}")
+print(f"Среднее время обработки транзакции: {avg_time:.2f} мс")
+print(f"Процент обнаружения невалидных транзакций: {detection_rate:.2f}%")
+
+df_summary= pd.DataFrame({
+  "Показатель": ["Всего транзакций", "Валидные транзакции", "Невалидные транзакции", "Среднее время обработки", "Процент обнаружения невалидных транзакций"], "Значение": [N, valid_count, invalid_count, f"{avg_time:.2f} мс", f"{detection_rate:.2f}%"]
+})
+print("\nСводная таблица:")
+print(df_summary.to_string(index=False))
+
+df_summary.to_csv("summary_table.csv", index=False, encoding="utf-8-sig")
+
+labels = ["Корректные транзакции", "Некорректные транзакции"]
+sizes = [valid_count, invalid_count]
+explode=(0.05,0.1)
 plt.figure()
-plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am2')
-plt.savefig('chart2_2.png')
-plt.figure()
+plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.title("Распределение транзакций")
+plt.savefig("pie_chart.png")
+plt.close()
 
-vals = Am_lst
-labels = [str(x) for x in range(1, 10)]
-#Все правильно str(x) for x in range(1, 7), 5
-explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1,0.1,0.1,0.1) #Что это означает? #Список из чисел #Почти правильно
-fig, ax = plt.subplots()
-ax.pie(vals,
-       labels=labels,
-       autopct='%1.1f%%',
-       shadow=True,
-       explode=explode,
-       wedgeprops={
-           'lw': 1,
-           'ls': '--',
-           'edgecolor': "k"
-       },
-       rotatelabels=True)
-ax.axis('equal')
-plt.savefig('chart2_3.png')
-plt.figure()
 
-vals = Am_lst_2
-labels = [str(x) for x in range(1, 10)] #Что это означает? #Список из чисел от одного до семи
-#Все правильно 5
-tt1=20
-ее2=40
-explode = (0.1, 0.1, 0.1, 0.1, 0.1,0.1, 0.1,0.1, 0.1)
-fig, ax = plt.subplots() 
-ax.pie(vals,
-       explode=explode,
-       labels=labels,
-       autopct='%1.1f%%',
-       shadow=True,
-       wedgeprops={
-           'lw': 1,
-           'ls': '--',
-           'edgecolor': "k"
-       },
-       rotatelabels=True)
-ax.axis('equal')
-plt.savefig('chart2_4.png')
-plt.figure()
 
-plt.figure()
-table1 = list(zip(Y, Am_lst))
-table2 = list(zip(Y, Am_lst_2))
-tfame = pd.DataFrame(table1, columns=['Y', 'Am_lst'])
-tfame2 = pd.DataFrame(table2, columns=['Y', 'Am_lst_2'])
-plt.bar(tfame['Y'], tfame['Am_lst'])
-plt.savefig('chart2_5.png')
-plt.figure()
-plt.bar(tfame['Y'], tfame2['Am_lst_2'])
-plt.savefig('chart2_6.png')
-plt.figure()
+
+print("График сохранен в файл pie_chart.png")
+
+
+# # ЗАДАНИЕ ЛР_3 СЕКРЕТНЫЕ КЛЮЧИ 1 часть
+# import os
+
+# my_secret_1 = os.environ["MY_SECRET"]
+# my_secret_2 = os.environ["MY_SECRET_2"]
+# my_secret_3 = os.environ["TAZHIBAEV_1"]
+# my_secret_4 = os.environ["TAZHIBAEV_2"]
+
+# print(my_secret_1, my_secret_2, my_secret_3, my_secret_4)
+
+# from sympy import *
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # 2 часть задания (Проверяет Виктор)
+# ##  ИНДИВИДУАЛЬНАЯ ЧАСТЬ ЗАДАНИЯ (3 Вариант)
+# k, T, C, L = symbols("k T C L")
+# C_ost = 50000
+# Am_lst, C_ost_lst = [], []
+# for i in range(9):
+#     Am = (C - L) / T  # Что это означает?
+#     # формула расчета платежа # Правильно
+#     val = float(Am.subs({C: 50000, T: 9, L: 0}))
+#     C_ost -= val
+#     Am_lst.append(round(val, 2))
+#     C_ost_lst.append(round(float(C_ost), 2))
+# print("Am_lst=", Am_lst)
+# print("C_ost_lst=", C_ost_lst)
+# # Все правильно 5, изменилось C_ost = 20000 и T = 6
+
+# # 2 способ
+# Aj = 0
+# C_ost = 50000
+# Am_lst_2 = []
+# C_ost_lst_2 = []
+# for i in range(9):
+#     Am = k * 1 / T * (C - Aj)
+#     C_ost -= Am.subs({C: 50000, T: 9, k: 2})
+#     Am_lst_2.append(round(Am.subs({C: 50000, T: 9, k: 2}), 2))
+#     Aj += Am
+#     C_ost_lst_2.append(round(C_ost, 2))
+# print("Am_lst_2=", Am_lst_2)
+# print("C_ost_lst_2=", C_ost_lst_2)
+# # Все правильно 5, изменилось C_ost = 20000 и T = 6
+
+
+# # Табличный вывод
+# Y = range(1, 7)  # Что это означает?
+# # цикл от 1 о 7  Правильно
+# table1 = list(zip(Y, C_ost_lst, Am_lst))
+# table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
+# tfame = pd.DataFrame(table1, columns=["Y", "C_ost_lst", "Am_lst"])
+# tfame2 = pd.DataFrame(table2, columns=["Y", "C_ost_lst_2", "Am_lst_2"])
+# print(tfame)
+# print(tfame2)
+
+# # Контейнер визуализации
+# plt.plot(tfame["Y"], tfame["C_ost_lst"], label="Am")
+# plt.savefig("chart2_1.png")
+# plt.figure()
+# plt.plot(tfame2["Y"], tfame2["C_ost_lst_2"], label="Am2")
+# plt.savefig("chart2_2.png")
+# plt.figure()
+
+# vals = Am_lst
+# labels = [str(x) for x in range(1, 10)]
+# # Все правильно str(x) for x in range(1, 7), 5
+# explode = (
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+#     0.1,
+# )  # Что это означает? #Список из чисел #Почти правильно
+# fig, ax = plt.subplots()
+# ax.pie(
+#     vals,
+#     labels=labels,
+#     autopct="%1.1f%%",
+#     shadow=True,
+#     explode=explode,
+#     wedgeprops={"lw": 1, "ls": "--", "edgecolor": "k"},
+#     rotatelabels=True,
+# )
+# ax.axis("equal")
+# plt.savefig("chart2_3.png")
+# plt.figure()
+
+# vals = Am_lst_2
+# labels = [
+#     str(x) for x in range(1, 10)
+# ]  # Что это означает? #Список из чисел от одного до семи
+# # Все правильно 5
+# tt1 = 20
+# ее2 = 40
+# explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+# fig, ax = plt.subplots()
+# ax.pie(
+#     vals,
+#     explode=explode,
+#     labels=labels,
+#     autopct="%1.1f%%",
+#     shadow=True,
+#     wedgeprops={"lw": 1, "ls": "--", "edgecolor": "k"},
+#     rotatelabels=True,
+# )
+# ax.axis("equal")
+# plt.savefig("chart2_4.png")
+# plt.figure()
+
+# plt.figure()
+# table1 = list(zip(Y, Am_lst))
+# table2 = list(zip(Y, Am_lst_2))
+# tfame = pd.DataFrame(table1, columns=["Y", "Am_lst"])
+# tfame2 = pd.DataFrame(table2, columns=["Y", "Am_lst_2"])
+# plt.bar(tfame["Y"], tfame["Am_lst"])
+# plt.savefig("chart2_5.png")
+# plt.figure()
+# plt.bar(tfame["Y"], tfame2["Am_lst_2"])
+# plt.savefig("chart2_6.png")
+# plt.figure()
---
+++ Сватков2.sh
@@ -0,0 +1,4 @@
+echo "Скрипт для ЛР_3_ИЗ"
+touch testfile
+ls
+echo "Конец скрипта ИЗ"
